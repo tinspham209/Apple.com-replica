3fe2918facd1ddb226374ccca7b853c89048bfef
quay189 <34814999+quay189@users.noreply.github.com>
2020-05-24 12:39:37 +0700

    Animations & Responsive for mobile devices

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 //JavaScript
+const selectElement = (element) => document.querySelector(element);
+
+selectElement(".mobile-menu").addEventListener("click", () => {
+  selectElement("header").classList.toggle("active");
+});
